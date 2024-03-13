3716dce0c3b28e87ee5cbe9b6e8cd5b2df9135ad
rafael-quixabeira <89958713+rafael-quixabeira@users.noreply.github.com>
2024-03-12 22:42:40 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.9
 //
 //  Package.swift
 //
@@ -17,7 +17,8 @@ let package = Package(
     .iOS(.v12),
     .macOS(.v10_13),
     .tvOS(.v12),
-    .watchOS(.v4)
+    .watchOS(.v4),
+    .visionOS(.v1)
   ],
   products: [
     // Products define the executables and libraries produced by a package, and make them visible to other packages.
